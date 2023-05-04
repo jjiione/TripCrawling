a258491df3e52bd51b2aab566865a380c8ea38d3
jiwon <jiwonjeong37@gmail.com>
2023-05-04 17:49:45 +0900

    submit

---
+++ src/main/java/com/ssafy/crawling/service/PlaceService.java
@@ -0,0 +1,30 @@
+package com.ssafy.crawling.service;
+
+import com.ssafy.crawling.reposiroty.PlaceRepository;
+import com.ssafy.crawling.entity.PlaceEntity;
+import com.ssafy.crawling.entity.PlaceDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@org.springframework.stereotype.Service
+public class PlaceService {
+    private PlaceRepository placeRepository;
+
+    @Autowired
+    public PlaceService(PlaceRepository placeRepository){
+        this.placeRepository = placeRepository;
+    }
+    public List<PlaceDto> placeList(){
+        List<PlaceEntity> places = placeRepository.findAll();
+        List<PlaceDto> placeDtos = new ArrayList<>();
+        for (PlaceEntity place : places) {
+            PlaceDto placeDto = new PlaceDto(place);
+            placeDtos.add(placeDto);
+        }
+        return placeDtos;
+    }
+
+
+}
